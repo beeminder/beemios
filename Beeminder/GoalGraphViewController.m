dc38eeccac7c3799f026b321d3b19f7deb6154dd
Andy Brett <andy@andybrett.com>
2012-08-04 16:46:08 -0700

    double tap on graph view

--- Beeminder/GoalGraphViewController.m
+++ Beeminder/GoalGraphViewController.m
@@ -13,9 +13,9 @@
 @end
 
 @implementation GoalGraphViewController
-@synthesize graphImageView;
-@synthesize graphImage = _graphImage;
-@synthesize scrollView;
+
+#define ZOOM_VIEW_TAG 100
+#define ZOOM_STEP 1.5
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -32,11 +32,25 @@
     if (self.graphImage) {
         [self.graphImageView setFrame:self.view.frame];
         [self.graphImageView setImage:self.graphImage];
-        self.scrollView.minimumZoomScale = 0.5;
-        self.scrollView.maximumZoomScale = 2.0;
+        self.scrollView.minimumZoomScale = 1.0;
+        self.scrollView.maximumZoomScale = 2.5;
         self.scrollView.clipsToBounds = YES;
         self.scrollView.contentSize = self.graphImage.size;
         self.scrollView.delegate = self;
+        
+        // set the tag for the image view
+        [self.graphImageView setTag:ZOOM_VIEW_TAG];
+        
+        // add gesture recognizers to the image view
+        UITapGestureRecognizer *singleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleSingleTap:)];
+        UITapGestureRecognizer *doubleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleDoubleTap:)];
+        UITapGestureRecognizer *twoFingerTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTwoFingerTap:)];
+        [doubleTap setNumberOfTapsRequired:2];
+        [twoFingerTap setNumberOfTouchesRequired:2];
+        
+        [self.graphImageView addGestureRecognizer:singleTap];
+        [self.graphImageView addGestureRecognizer:doubleTap];
+        [self.graphImageView addGestureRecognizer:twoFingerTap];
     }
 
 	// Do any additional setup after loading the view.
@@ -64,6 +78,43 @@
 - (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
 {
     return self.graphImageView;
+}
+
+#pragma mark TapDetectingImageViewDelegate methods
+
+- (void)handleSingleTap:(UIGestureRecognizer *)gestureRecognizer {
+    // single tap does nothing for now
+}
+
+- (void)handleDoubleTap:(UIGestureRecognizer *)gestureRecognizer {
+    // double tap zooms in
+    float newScale = [self.scrollView zoomScale] * ZOOM_STEP;
+    CGRect zoomRect = [self zoomRectForScale:newScale withCenter:[gestureRecognizer locationInView:gestureRecognizer.view]];
+    [self.scrollView zoomToRect:zoomRect animated:YES];
+}
+
+- (void)handleTwoFingerTap:(UIGestureRecognizer *)gestureRecognizer {
+    // two-finger tap zooms out
+    float newScale = [self.scrollView zoomScale] / ZOOM_STEP;
+    CGRect zoomRect = [self zoomRectForScale:newScale withCenter:[gestureRecognizer locationInView:gestureRecognizer.view]];
+    [self.scrollView zoomToRect:zoomRect animated:YES];
+}
+
+- (CGRect)zoomRectForScale:(float)scale withCenter:(CGPoint)center {
+    
+    CGRect zoomRect;
+    
+    // the zoom rect is in the content view's coordinates.
+    //    At a zoom scale of 1.0, it would be the size of the imageScrollView's bounds.
+    //    As the zoom scale decreases, so more content is visible, the size of the rect grows.
+    zoomRect.size.height = [self.scrollView frame].size.height / scale;
+    zoomRect.size.width  = [self.scrollView frame].size.width  / scale;
+    
+    // choose an origin so as to get the right center.
+    zoomRect.origin.x    = center.x - (zoomRect.size.width  / 2.0);
+    zoomRect.origin.y    = center.y - (zoomRect.size.height / 2.0);
+    
+    return zoomRect;
 }
 
 @end
