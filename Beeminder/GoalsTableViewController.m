08f41f2382607a24e3c45e4b5ead6e62a7435bfc
Andy Brett <andy@andybrett.com>
2012-11-09 12:24:53 -0500

    fix crasher

--- Beeminder/GoalsTableViewController.m
+++ Beeminder/GoalsTableViewController.m
@@ -229,23 +229,25 @@
         [modGoalDict setObject:[goalDict objectForKey:@"id"] forKey:@"serverId"];
         NSString *runits = [goalDict objectForKey:@"runits"];
         NSNumber *weeklyRate;
-        if ([runits isEqualToString:@"y"]) {
-            weeklyRate = [NSNumber numberWithDouble:[[goalDict objectForKey:@"rate"] doubleValue]/52];
+        if ([goalDict objectForKey:@"rate"] != (id)[NSNull null]) {
+
+            if ([runits isEqualToString:@"y"]) {
+                weeklyRate = [NSNumber numberWithDouble:[[goalDict objectForKey:@"rate"] doubleValue]/52];
+            }
+            else if ([runits isEqualToString:@"m"]) {
+                weeklyRate = [NSNumber numberWithDouble:[[goalDict objectForKey:@"rate"] doubleValue]/4];
+            }
+            else if ([runits isEqualToString:@"d"]) {
+                weeklyRate = [NSNumber numberWithDouble:[[goalDict objectForKey:@"rate"] doubleValue]*7];
+            }
+            else if ([runits isEqualToString:@"h"]) {
+                weeklyRate = [NSNumber numberWithDouble:[[goalDict objectForKey:@"rate"] doubleValue]*7*24];
+            }
+            else {
+                weeklyRate = [goalDict objectForKey:@"rate"];
+            }
+            [modGoalDict setObject:weeklyRate forKey:@"rate"];
         }
-        else if ([runits isEqualToString:@"m"]) {
-            weeklyRate = [NSNumber numberWithDouble:[[goalDict objectForKey:@"rate"] doubleValue]/4];
-        }
-        else if ([runits isEqualToString:@"d"]) {
-            weeklyRate = [NSNumber numberWithDouble:[[goalDict objectForKey:@"rate"] doubleValue]*7];
-        }
-        else if ([runits isEqualToString:@"h"]) {
-            weeklyRate = [NSNumber numberWithDouble:[[goalDict objectForKey:@"rate"] doubleValue]*7*24];
-        }
-        else {
-            weeklyRate = [goalDict objectForKey:@"rate"];
-        }
-        
-        [modGoalDict setObject:weeklyRate forKey:@"rate"];
         
         Goal *goal = [Goal writeToGoalWithDictionary:modGoalDict forUserWithUsername:[ABCurrentUser username]];
         [self.goalObjects addObject:goal];
